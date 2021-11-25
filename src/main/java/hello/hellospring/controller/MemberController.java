c9a678a07577c1c867ac6ec40fd168539c81ccd1
DevKS-Yet <ksang0324@gmail.com>
2021-11-25 23:24:43 +0900

    20211125-의존성주입

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,17 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
 
     private final MemberService memberService;
-    // private final MemberService memberService = new MemberService();
+//    private final MemberService memberService = new MemberService();
+
+    // 필드주입
+//    @Autowired private MemberService memberService;
+
+    //세터 주입
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+    //  예전에는 많이 사용했으나 지금은 많이 사용하지 않음.
 
     @Autowired
     public MemberController(MemberService memberService) {
