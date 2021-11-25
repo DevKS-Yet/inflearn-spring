05136fc015ced21c3997b3fd82d4c72239c14ba9
DevKS-Yet <ksang0324@gmail.com>
2021-11-25 23:06:41 +0900

    20211125 @Component

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Demo {
+}
+
+// 이것은 스프링 빈에 등록되지는 않는다. 따로 설정을 해야함.
+// 설정되는 것은 프로젝트명Application 이하에 있는 것들만 기본으로 등록하게 됨
